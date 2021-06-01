472ec0e2a4a6c7bf254eab0065b5e1ea7d2253db
Rum <muhammadrum135@gmail.com>
2021-06-02 07:55:58 +0800

    Materi Hukum Done

--- app/src/main/java/com/afila/energiusaha/menu/materi/hukum/HukumFragment.java
+++ app/src/main/java/com/afila/energiusaha/menu/materi/hukum/HukumFragment.java
@@ -1,5 +1,6 @@
 package com.afila.energiusaha.menu.materi.hukum;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,52 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.afila.energiusaha.R;
+import com.afila.energiusaha.menu.materi.WadahVideoActivity;
+import com.afila.energiusaha.menu.materi.usaha.UsahaLatihanActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HukumFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HukumFragment extends Fragment {
+    private TextView tvToolbar;
+    private String toolbarText;
+    private ImageView toolbarback;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View btnVideo,btnLatihan;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Intent intent;
 
-    public HukumFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HukumFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HukumFragment newInstance(String param1, String param2) {
-        HukumFragment fragment = new HukumFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hukum, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_hukum, container, false);
+
+        btnLatihan = root.findViewById(R.id.btn_latihan);
+        btnVideo = root.findViewById(R.id.btn_video);
+
+//        tvToolbar.setText("");
+
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getActivity(), WadahVideoActivity.class);
+                intent.putExtra("idvideo","-VAc_RjMoyA");
+                startActivity(intent);
+            }
+        });
+
+        btnLatihan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getActivity(), UsahaLatihanActivity.class);
+                intent.putExtra("toolbarText","Latihan-Hukum 1");
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 }
